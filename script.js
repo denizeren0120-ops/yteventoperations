754325fff8da659ac24312cda85f1baac17e829b
denizeren0120-ops <deniz.er.en0120@gmail.com>
2026-08-02 17:31:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('YT Event Operations hazır.');
+window.addEventListener("load", () => {
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+        document.body.style.transition = "opacity .8s";
+        document.body.style.opacity = "1";
+    }, 100);
+});
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+});
+
+cards.forEach((card) => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
+    card.style.transition = "0.6s";
+    observer.observe(card);
+});
